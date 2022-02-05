6c9ecededba3a2ad79b5ebd7e24b26b90b8056e5
AdrielGenao <86543251+AdrielGenao@users.noreply.github.com>
2022-02-04 22:59:25 -0500

    Working on and understanding flex layout
    Continuing to learn the concept of the flex layout system of React Native, and using borders for flex boxes makes it much easier to see the size of boxes and use this to compare sizes of child components.

--- CapelliApp/CapelliApp.js
+++ CapelliApp/CapelliApp.js
@@ -11,16 +11,20 @@ import {
 } from 'react-native';
 import Constants from 'expo-constants';
 import lines from './assets/lines.png';
+import capelliLogo from './assets/capellibeauty.png';
 
 export default function App() {
-  const [buttonView, buttonChange] = useState(false);
-  const [textSay, textChange] = useState('');
+  const [buttonView, buttonChange] = useState(false); // Checking if three-lined button is pressed
+  const [textSay, textChange] = useState(''); // Text state variable
 
-  const renderItem = ({ item }) => (
+  const renderItem = (
+    { item } // Buttons for FlatList rendering
+  ) => (
     <Pressable
       onPress={() => {
         {
           if (textSay != item.label) {
+            // Changes the textSay variable based on which button is pressed
             textChange(item.label);
           } else {
             textChange('');
@@ -33,43 +37,85 @@ export default function App() {
   );
 
   return (
-    <View style={styles.container}>
-      {buttonView ? (
-        <View style={styles.listView}>
-          <FlatList
-            data={[
-              { label: 'Featured Products' },
-              { label: 'New Additions' },
-              { label: 'Brands' },
-            ]}
-            renderItem={renderItem}
-          />
+    <>
+      {/*View for all components on home page*/}
+      <View style={styles.allViews}>
+        {/*View for pop-up button list*/}
+        <View style={buttonView ? styles.buttonListOpen : styles.buttonListClosed}>
+          {buttonView ? (
+              <FlatList // FlatList for list of buttons to select from
+                data={[
+                  { label: 'Featured Products' },
+                  { label: 'New Additions' },
+                  { label: 'Brands' },
+                ]}
+                renderItem={renderItem}
+              />
+          ) : null}
         </View>
-      ) : null}
-      <Pressable
-        onPress={() => {
-          buttonChange(!buttonView);
-        }}
-        style={styles.openListButton}>
-        <Image source={lines} style={styles.openListButtonImage} />
-      </Pressable>
-      <Text style={styles.text}>{textSay}</Text>
-    </View>
+        {/*View for main home page without button-list: just the title and body of homepage*/}
+        <View style={styles.titleBody}>
+          {/*View for title flexbox*/}
+          <View style={styles.title}>
+            <Pressable
+              onPress={() => {
+                buttonChange(!buttonView); // Changes state variable of if the three-lined button is pressed or not
+              }}
+              style={styles.openListButton}>
+              <Image source={lines} style={styles.openListButtonImage} />
+            </Pressable>
+            <Image source={capelliLogo} style={styles.capelliLogoImage} />
+          </View>
+          {/*View for body flexbox*/}
+          <View style={styles.body}></View>
+        </View>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  // Styles for all components
+  allViews: {
     flexDirection: 'row',
+    borderWidth: 2,
+    borderColor: "yellow",
+    height:"100%",
+    width:"100%"
   },
-  listView: {
-    marginTop: 23,
-    width: 150,
+  buttonListClosed: {
+    flexDirection: 'column',
+    flex: 0,
+  },
+  buttonListOpen: {
+    flexDirection: 'column',
+    flex: 2,
+    borderWidth: 2,
+    borderColor: "black",
+    marginTop: '10%',
+  },
+  titleBody: {
+    flexDirection: 'column',
+    flex: 3,
+    borderWidth: 1,
+    borderColor: "blue"
+  },
+  title: {
+    flexDirection: 'row',
+    flex: 1,
+    borderWidth: 1,
+    borderColor: "green"
+  },
+  body: {
+    flexDirection: 'row',
+    flex: 3,
+    borderWidth: 1,
+    borderColor: "red"
   },
   listButton: {
     borderWidth: 1,
-    width: 150,
-    height: 50,
+    width: "100%",
+    height: "100%",
     justifyContent: 'center',
   },
   listButtonText: {
@@ -78,19 +124,21 @@ const styles = StyleSheet.create({
   },
   openListButton: {
     borderWidth: 1,
-    width: 40,
-    height: 40,
-    marginTop: 23,
-    marginLeft: 7,
+    width: 30,
+    height: 35,
+    marginTop: 25,
+    marginLeft: 17,
   },
   openListButtonImage: {
     resizeMode: 'stretch',
-    width: 40,
-    height: 40,
+    width: '100%',
+    height: '100%',
   },
-  text: {
-    marginTop: 23,
-    marginLeft: 20,
-    fontSize: 25,
+  capelliLogoImage: {
+    resizeMode: 'stretch',
+    marginTop: '9%',
+    marginLeft: '20%',
+    width: 120,
+    height: 100,
   },
 });
